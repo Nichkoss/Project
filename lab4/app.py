146e4aeea36d3bb8c8f648433e3da653c58b1f49
Nichkoss <lanchuvvv@gmail.com>
2022-09-25 15:19:00 +0300

    Added new files

---
+++ lab4/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from wsgiref.simple_server import make_server
+
+from blueprint import api_blueprint
+
+app = Flask(__name__)
+
+with make_server('', 5000, app) as server:
+    app.register_blueprint(api_blueprint, url_prefix="/api/v1")
+    server.serve_forever()
+
+
+# curl -v -XGET http://localhost:5000/api/v1/hello-world-13
